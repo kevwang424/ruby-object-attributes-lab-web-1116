be0cb122b148588f02fc41882c6336f3a5ce0ea9
kevwang424 <kev.wang424@gmail.com>
2016-11-29 21:29:55 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name=(dog_name)   #this is the setter method as we assign
+    @name = dog_name
+  end
+
+  def name            #this is the getter method as we retrieve
+    @name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
+end
